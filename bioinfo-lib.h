420c4b0b2b3c3fc90bd33cc6461145ab9be45299
Peter Rupa <megatricycle@gmail.com>
2016-03-07 22:35:56 +0800

    Add getHammingDistance

--- bioinfo-lib.h
+++ bioinfo-lib.h
@@ -1,3 +1,27 @@
+#include<string.h>
+
+/*
+    Returns the the number of characters that differ in ith position from position 1 to strlen(str1).
+    
+    Returns -1 on different length error.
+*/
 int getHammingDistance(char *str1, char *str2) {
-    return 1;
+    int distance = 0;
+    int i;
+    
+    // check if same length
+    if(strlen(str1) != strlen(str2)) {
+        return -1;
+    }
+    
+    // check each element if not equal
+    for(i = 0; i < strlen(str1); i++) {
+        // increment distance when not equal
+        if(str1[i] != str2[i]) {
+            distance++;
+        }
+    }
+    
+    return distance;
 }
+
